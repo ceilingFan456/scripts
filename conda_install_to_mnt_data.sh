8ede2ee36e14d23b911a23b0824b373569d0b9b8
Huang Qiming <t-qimhuang@microsoft.com>
2025-10-06 01:31:25 +0000

    Update conda script

--- conda_install_to_mnt_data.sh
+++ conda_install_to_mnt_data.sh
@@ -2,33 +2,33 @@
 set -e
 
 # Install location (persistent storage)
-INSTALL_DIR="$HOME/disk/miniconda3"
+# INSTALL_DIR="$HOME/disk"
+INSTALL_DIR="$HOME"
 
 # Clean up any previous install
-rm -rf "$INSTALL_DIR"
+rm -rfI "$INSTALL_DIR"
 
 # Download latest Miniconda installer
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /tmp/miniconda.sh
 
 # Install Miniconda silently
-bash /tmp/miniconda.sh -b -p "$INSTALL_DIR"
+bash /tmp/miniconda.sh -b -u "$INSTALL_DIR"
 
 # Add conda to PATH in ~/.bashrc
-if ! grep -q "$INSTALL_DIR/bin" ~/.bashrc; then
-  echo 'export PATH="'"$INSTALL_DIR"'/bin:$PATH"' >> ~/.bashrc
+if ! grep -q "$INSTALL_DIR/miniconda3/bin" ~/.bashrc; then
+  echo 'export PATH="'"$INSTALL_DIR/miniconda3/bin:$PATH"'"' >> ~/.bashrc
 fi
 
 # Apply changes to current shell
-export PATH="$INSTALL_DIR/bin:$PATH"
-
+export PATH="$INSTALL_DIR/miniconda3/bin:$PATH"
 # Verify install
 conda --version
 
 # 1) Load Conda’s shell hook now (enables `conda create/activate` in THIS shell)
-eval "$($HOME/disk/miniconda3/bin/conda shell.bash hook)"
+eval "$($INSTALL_DIR/miniconda3/bin/conda shell.bash hook)"
 
 # 2) Make it permanent for future shells
-$HOME/disk/miniconda3/bin/conda init bash
+$INSTALL_DIR/miniconda3/bin/conda init bash
 
 source ~/.bashrc
 
