c534da9fb4df72929d602b2858c5300aa6fed6ef
Huang Qiming <e0271228@u.nus.edu>
2026-01-26 03:33:19 +0000

    Update scripts

---
+++ conda_env_list.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+## biomedparse can run biomedparse-v2 and medevalkit together 
+conda activate biomedparse
+
+## eval can run medevalkit and showo 
+conda activate eval
+
+## openpi is for pi 0.5
+conda activate openpi
+
