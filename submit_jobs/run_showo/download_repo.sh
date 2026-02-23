e269a83ed3e21a185ceaf1500039e93d9fd16031
Huang Qiming <e0271228@u.nus.edu>
2026-02-23 17:49:38 +0000

    Update run showo scripts

--- submit_jobs/run_showo/download_repo.sh
+++ submit_jobs/run_showo/download_repo.sh
@@ -10,3 +10,9 @@ git clone https://$GITHUB_TOKEN@github.com/ceilingFan456/MedEValKit-dev.git
 
 echo "Cloning MedSAM repository..."
 git clone https://$GITHUB_TOKEN@github.com/ceilingFan456/MedSAM.git
+
+# apt-get update && apt-get install -y software-properties-common
+# add-apt-repository universe
+# sudo apt install tmux -y
+
+pip uninstall uv
